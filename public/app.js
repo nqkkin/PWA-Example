dbee83188b9c7870f51d9a17e157adbd35a6e9f0
nikkin <nilsenn@alfredstate.edu>
2024-03-28 00:51:01 -0400

    Base Made for Class
    Base Made for Class that has the manifest file but not working service worker.

--- public/app.js
+++ public/app.js
@@ -25,25 +25,8 @@ window.onload = function() {
     document.getElementById('todo-form').onsubmit = addTodo;
 };
 
-// Function to register the service worker
-const registerServiceWorker = async () => {
-    if ("serviceWorker" in navigator) {
-        try {
-            const registration = await navigator.serviceWorker.register("/service-worker.js", {
-                scope: "/"
-            });
-            if (registration.installing) {
-                console.log("Service worker installing");
-            } else if (registration.waiting) {
-                console.log("Service worker installed");
-            } else if (registration.active) {
-                console.log("Service worker active");
-            }
-        } catch (error) {
-            console.error(`Registration failed with ${error}`);
-        }
-    }
-};
+// Function to register the service worker will be here
+
 
 function addTodo(e) {
     e.preventDefault();
--- server.js
+++ server.js
@@ -7,11 +7,10 @@ const PORT = process.env.PORT || 3000; // Set port to 3000 or process.env.PORT (
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*Catch-all route to serve the index.html file
+// Catch-all route to serve the index.html file
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-*/
 
 // Start the server
 app.listen(PORT, () => {
